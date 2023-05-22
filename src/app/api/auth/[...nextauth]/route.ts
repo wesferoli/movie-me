1d0f1afc22677402e5b46d2f21fa6879fa7ea7ab
wesferoli <wesleyfer.oli97@gmail.com>
2023-05-22 14:43:28 -0300

    feat(prisma/next-auth): create user when first login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import prisma from "@/lib/prisma";
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
@@ -8,6 +9,32 @@ const handler = NextAuth({
       clientSecret: String(process.env.GITHUB_SECRET),
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { id, email, image, name } = user;
+
+      if (email) {
+        const dbUser = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        if (!dbUser) {
+          await prisma.user.create({
+            data: {
+              email: email,
+              avatarUrl: image || "",
+              name: name || "",
+              login: id,
+            },
+          });
+        }
+
+        return true;
+      }
+
+      return false;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
