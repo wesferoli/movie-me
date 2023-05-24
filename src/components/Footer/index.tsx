28663d3852bba0f26380a2e6d826d5e056e0e4d1
wesferoli <wesleyfer.oli97@gmail.com>
2023-05-24 14:57:08 -0300

    feat(footer): responsive component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,25 +4,25 @@ import LinkedinIcon from "public/linkedin.png";
 
 export default function Footer() {
   return (
-    <footer className="absolute bottom-0 w-full border-t-2 border-yellow-500 bg-neutral-700 px-24 py-4">
-      <div className="grid grid-cols-8 divide-x divide-yellow-500">
-        <div className="col-span-2 flex items-center justify-center gap-10 px-4 py-2">
+    <footer className="absolute bottom-0 w-full border-t-2 border-yellow-500 bg-neutral-700 px-5 py-4 sm:px-10 lg:px-24">
+      <div className="grid grid-cols-1 divide-yellow-500 max-sm:divide-y sm:grid-cols-8 sm:divide-x sm:py-2">
+        <div className="flex items-center justify-center gap-6 py-4 sm:col-span-2 sm:px-4 lg:gap-10">
           <a href="https://github.com/wesferoli" target="_blank">
             <Image
               src={GithubIcon}
               alt="My github"
-              className="h-[50px] w-[50px]"
+              className="h-[40px] w-[40px] md:h-[50px] md:w-[50px]"
             />
           </a>
           <a href="https://linkedin.com/in/wesferoli" target="_blank">
             <Image
               src={LinkedinIcon}
               alt="My linkedin"
-              className="h-[50px] w-[50px]"
+              className="h-[40px] w-[40px] md:h-[50px] md:w-[50px]"
             />
           </a>
         </div>
-        <div className="col-span-4 flex items-center justify-center px-4">
+        <div className="flex items-center justify-center py-4 sm:col-span-4 sm:px-4">
           <span className="text-center">
             Icons created by{" "}
             <a
@@ -35,7 +35,7 @@ export default function Footer() {
             </a>
           </span>
         </div>
-        <div className="col-span-2 flex items-center justify-center px-4">
+        <div className="flex items-center justify-center py-4 sm:col-span-2 sm:px-4">
           <span className="text-center">
             See this project on{" "}
             <a
