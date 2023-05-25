3a228682d6f6050f44c126a60cc0d9a3bd252789
wesferoli <wesleyfer.oli97@gmail.com>
2023-05-25 14:45:38 -0300

    fix(footer): add some size changes to responsive

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,21 +4,21 @@ import LinkedinIcon from "public/linkedin.png";
 
 export default function Footer() {
   return (
-    <footer className="absolute bottom-0 w-full border-t-2 border-yellow-500 bg-neutral-700 px-5 py-4 sm:px-10 lg:px-24">
-      <div className="grid grid-cols-1 divide-yellow-500 max-sm:divide-y sm:grid-cols-8 sm:divide-x sm:py-2">
+    <footer className="absolute bottom-0 w-full border-t-2 border-yellow-500 bg-neutral-700 px-4 sm:px-10 sm:py-2 lg:px-24">
+      <div className="grid grid-cols-1 divide-yellow-500 text-sm max-sm:divide-y sm:grid-cols-8 sm:divide-x sm:py-2 md:text-base">
         <div className="flex items-center justify-center gap-6 py-4 sm:col-span-2 sm:px-4 lg:gap-10">
           <a href="https://github.com/wesferoli" target="_blank">
             <Image
               src={GithubIcon}
               alt="My github"
-              className="h-[40px] w-[40px] md:h-[50px] md:w-[50px]"
+              className="h-[35px] w-[35px] md:h-[45px] md:w-[45px]"
             />
           </a>
           <a href="https://linkedin.com/in/wesferoli" target="_blank">
             <Image
               src={LinkedinIcon}
               alt="My linkedin"
-              className="h-[40px] w-[40px] md:h-[50px] md:w-[50px]"
+              className="h-[35px] w-[35px] md:h-[45px] md:w-[45px]"
             />
           </a>
         </div>
