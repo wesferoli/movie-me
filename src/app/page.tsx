3add82ba214e6d751faab61be980363a4e003192
wesferoli <wesleyfer.oli97@gmail.com>
2023-05-30 16:08:22 -0300

    feat(poster): create poster component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Poster from "@/components/Poster";
+
 export default function Home() {
+  const movie = {
+    title: "John Wick: Chapter 4",
+    releaseYear: "2023",
+    poster: "https://image.tmdb.org/t/p/w500/vZloFAK7NmvMGKE7VkF5UHaz0I.jpg",
+  };
+
   return (
-    <main>
-      <h1 className="text-2xl font-bold">Home</h1>
+    <main className="my-6 flex items-center justify-center md:my-20">
+      <Poster
+        posterUrl={movie.poster}
+        releaseYear={movie.releaseYear}
+        title={movie.title}
+      />
     </main>
   );
 }
---
+++ src/components/Poster/index.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface IPosterProps {
+  posterUrl: string;
+  title: string;
+  releaseYear: string;
+}
+
+export default function Poster({
+  posterUrl,
+  title,
+  releaseYear,
+}: IPosterProps) {
+  return (
+    <div className="relative w-max shadow-[0_0_15px_-3px] shadow-yellow-500">
+      <Link href="/">
+        <Image
+          className="w-40 sm:w-[180px] md:w-[200px] lg:w-[230px]"
+          alt={title}
+          src={posterUrl}
+          width={500}
+          height={750}
+        />
+        <div className="absolute bottom-0 h-full w-full transition delay-100 ease-in-out hover:opacity-100 md:opacity-0">
+          <div className="absolute bottom-0 h-1/2 w-full bg-gradient-to-t from-[rgba(248,217,139,0.6)] to-[rgba(0,0,0,0)]">
+            <div className="absolute bottom-4 left-3 right-3">
+              <h1 className="text-base font-bold text-white drop-shadow-[0_0_5px_rgba(0,0,0,0.75)] lg:text-lg">
+                {title}
+              </h1>
+              <p className="text-xs font-semibold text-zinc-300 drop-shadow-[0_0_5px_rgba(0,0,0,0.75)] lg:text-sm">
+                {releaseYear}
+              </p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
