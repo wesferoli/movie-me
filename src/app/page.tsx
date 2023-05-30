a92bf173291419adf62c75436dd95c8931fb70c5
wesferoli <wesleyfer.oli97@gmail.com>
2023-05-30 20:03:51 -0300

    fix(poster): mobile size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   const { data: movieList } = use(getMoviesList());
 
   return (
-    <main className="mx-16 my-8 flex flex-wrap justify-center gap-x-8 gap-y-6 sm:my-14 sm:gap-x-14 md:mx-24 md:gap-y-10 lg:mx-40 lg:my-20">
+    <main className="mx-10 my-8 flex flex-wrap justify-center gap-x-8 gap-y-6 sm:my-14 sm:gap-x-14 md:mx-24 md:gap-y-10 lg:mx-40 lg:my-20">
       {movieList.map((movie) => (
         <Poster
           posterUrl={movie.poster}
