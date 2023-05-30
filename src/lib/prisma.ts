139c9f242730ec168df3d75e6ce82728e58f3d6a
wesferoli <wesleyfer.oli97@gmail.com>
2023-05-30 14:26:08 -0300

    fix(prisma): change db to vercel postgres

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,16 @@
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+let prisma: PrismaClient;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalForPrisma.prisma;
+}
 
 export default prisma;
