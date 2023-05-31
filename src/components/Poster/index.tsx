deb7076d42f3895617d1fb3000ee7a718e226c59
wesferoli <wesleyfer.oli97@gmail.com>
2023-05-31 15:50:25 -0300

    refactor(poster): extend custom tailwind styles

--- src/components/Poster/index.tsx
+++ src/components/Poster/index.tsx
@@ -15,7 +15,7 @@ export default function Poster({
   movieId,
 }: IPosterProps) {
   return (
-    <div className="relative w-max shadow-[0_0_15px_-3px] shadow-yellow-500">
+    <div className="relative w-max shadow-poster">
       <Link href={`/movie/${movieId}`}>
         <Image
           className="min-w-[160px] max-w-[210px] md:max-w-[230px]"
@@ -24,16 +24,14 @@ export default function Poster({
           width={500}
           height={750}
         />
-        <div className="absolute bottom-0 h-full w-full transition delay-100 ease-in-out hover:opacity-100 md:opacity-0">
-          <div className="absolute bottom-0 h-1/2 w-full bg-gradient-to-t from-[rgba(248,217,139,1)] to-[rgba(0,0,0,0)]">
-            <div className="absolute bottom-4 left-3 right-3">
-              <h1 className="text-base font-bold text-white drop-shadow-[0_0_5px_rgba(0,0,0,0.75)] lg:text-lg">
-                {title}
-              </h1>
-              <p className="text-xs font-semibold text-zinc-300 drop-shadow-[0_0_5px_rgba(0,0,0,0.75)] lg:text-sm">
-                {releaseYear}
-              </p>
-            </div>
+        <div className="absolute bottom-0 h-full w-full bg-gradientYellow transition delay-100 ease-in-out hover:opacity-100 md:opacity-0">
+          <div className="absolute bottom-4 left-3 right-3">
+            <h1 className="text-base font-bold drop-shadow-text lg:text-lg">
+              {title}
+            </h1>
+            <p className="text-xs font-semibold text-zinc-300 drop-shadow-text lg:text-sm">
+              {releaseYear}
+            </p>
           </div>
         </div>
       </Link>
