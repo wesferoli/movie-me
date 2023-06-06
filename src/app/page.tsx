b883b080f9e8bf214b3bb29dde5720dae3a92a9a
wesferoli <wesleyfer.oli97@gmail.com>
2023-06-06 17:50:35 -0300

    refactor(movie-list): async component and get year helper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Poster from "@/components/Poster";
 import { api } from "@/lib/api";
-import { use } from "react";
 import { MovieList } from "./api/movie/types";
 
-async function getMoviesList() {
-  const response = await api.get<{ data: MovieList }>("/movie");
-  return response.data;
-}
-
-export default function Home() {
-  const { data: movieList } = use(getMoviesList());
+export default async function Home() {
+  const { data: movieList } = await api
+    .get<{ data: MovieList }>("/movie")
+    .then((resp) => resp.data);
 
   return (
     <main className="mx-10 my-8 flex flex-wrap justify-center gap-x-8 gap-y-6 sm:my-14 sm:gap-x-14 md:mx-24 md:gap-y-10 lg:mx-40 lg:my-20">
