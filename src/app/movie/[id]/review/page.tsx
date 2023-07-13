5123fac911c43d57d01e42c6f677e4e5c2989a8a
wesferoli <wesleyfer.oli97@gmail.com>
2023-07-13 18:41:38 -0300

    feat(create-review): add react hook form

--- src/app/movie/[id]/review/page.tsx
+++ src/app/movie/[id]/review/page.tsx
@@ -1,15 +1,9 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
-import { ApiCreateReview } from "@/app/api/review/actions";
-import { CreateReviewData } from "@/app/api/review/types";
 import { IRouteParams } from "@/app/api/types";
-import Button from "@/components/Button";
-import { Form } from "@/components/Form";
-import Rating from "@/components/Rating";
 import { getMovieDetails } from "@/services/api/movie";
 import { getServerSession } from "next-auth";
-import { revalidatePath } from "next/cache";
 import Image from "next/image";
-import { redirect } from "next/navigation";
+import FormCreateReview from "./FormCreateReview";
 
 export default async function CreateReview({ params }: IRouteParams) {
   const session = await getServerSession(authOptions);
@@ -17,20 +11,6 @@ export default async function CreateReview({ params }: IRouteParams) {
   const movieId = params.id;
 
   const { data: movie } = await getMovieDetails(params.id);
-
-  // Server Action
-  async function createReview(data: FormData) {
-    "use server";
-    data.append("userId", userId);
-    data.append("movieId", movieId);
-    const formData = Object.fromEntries(data.entries()) as CreateReviewData;
-
-    await ApiCreateReview(formData);
-
-    const redirectPath = `/movie/${movieId}`;
-    revalidatePath(redirectPath);
-    redirect(redirectPath);
-  }
 
   return (
     <div className="mx-4 my-8 max-w-[1136px] flex-grow rounded-lg border border-yellow-500 bg-neutral-700 p-2 sm:mx-10 sm:p-4 md:mx-10 md:p-10 lg:mx-24 lg:my-10 lg:p-10 xl:p-16">
@@ -56,25 +36,7 @@ export default async function CreateReview({ params }: IRouteParams) {
           </div>
         </div>
       </div>
-      <Form.Wrapper action={createReview} className="mt-2 space-y-2 md:mt-4">
-        <Form.Group>
-          <Form.Label>Rating</Form.Label>
-          <Rating />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label htmlFor="title-input">Title</Form.Label>
-          <Form.Input name="title" type="text" id="title-input" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label htmlFor="description-input">Description</Form.Label>
-          <Form.Textarea name="description" rows={4} id="description-input" />
-        </Form.Group>
-        <div className="flex justify-center pt-2">
-          <Button className="w-1/3 max-w-[170px]" variant="primary">
-            Create
-          </Button>
-        </div>
-      </Form.Wrapper>
+      <FormCreateReview movieId={movieId} userId={userId} />
     </div>
   );
 }
